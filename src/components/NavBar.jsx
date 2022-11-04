a61039c2d364c85c5dd35f334c30ed91a29bb3bf
Natnael Kibru <nathnaelkibru@gmail.com>
2022-11-04 14:37:57 +0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+        <div>
+            <h1 className='text-5xl font-sign ml-2'>Natik</h1>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
