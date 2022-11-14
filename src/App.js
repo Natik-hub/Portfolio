81fe4997e38fafd94f0bd52ddc2f9500ed339f60
Natnael Kibru <nathnaelkibru@gmail.com>
2022-11-14 14:55:57 +0300

    lastpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import About from './components/About';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Portfolio from './components/Portfolio';
@@ -14,6 +16,8 @@ function App() {
      <SocialLinks />
      <About />
      <Portfolio />
+     <Experience />
+     <Contact />
     </div>
   );
 }
